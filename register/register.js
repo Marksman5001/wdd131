52a4c491b8132a7530a092d02bae203ac7f1de58
Marksman5001 <marksman5001@byui.edu>
2025-03-27 21:31:01 -0600

    idk a bunch of stuff, also personal site started

---
+++ register/register.js
@@ -0,0 +1,9 @@
+let participants = 1;
+const button = document.querySelector("#add")
+button.addEventListener("click", function(){
+    participants++;
+    participantTemplate(participants);
+})
+function participantTemplate(count){
+
+}
